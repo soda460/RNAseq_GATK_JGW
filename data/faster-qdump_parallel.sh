746f16699e39dffba3f3f75234bd0effa02fd27c
Jean-Simon Brouard <jean-simon.brouard@canada.ca>
2021-04-16 11:59:26 -0400

    add faster-qdump_parallel.sh

---
+++ data/faster-qdump_parallel.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#$ -V
+#$ -N fasterq-dump
+#$ -S /bin/bash
+#$ -cwd
+#$ -j y
+#$ -b n
+#$ -e ./qsub_err.txt
+#$ -o ./qsub_log.txt
+#$ -q all.q
+#$ -t 1-24
+#$ -pe smp 4
+
+input=$(head -n $SGE_TASK_ID SRR.list | tail -n 1)
+
+
+fasterq-dump --split-files $input/$input.sra
